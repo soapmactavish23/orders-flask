518d242c6ae632c547affa00d6f5215ed1924dd6
soapmactavish23 <ricknogueira1231@gmail.com>
2026-05-21 18:38:12 -0300

    Acessando rota de registro de pedidos

--- src/main/routes/delivery_routes.py
+++ src/main/routes/delivery_routes.py
@@ -1,14 +1,15 @@
 from flask import Blueprint, request, jsonify
 
+from src.main.composer.registry_order_composer import registry_order_composer
 from src.main.http_types.http_request import HttpRequest
 
 delivery_routes_bp = Blueprint('delivery_routes', __name__)
 
 @delivery_routes_bp.route('/delivery/order', methods=['POST'])
 def registry_order():
-    print(request.json)
+    use_case = registry_order_composer()
     http_request = HttpRequest(body=request.json)
 
-    # enviar o http_request para a nossa lógica
+    response = use_case.registry(http_request)
 
-    return jsonify({"ola": "mundo"}), 200
+    return jsonify(response.body), response.status_code
